692eaff6cab783bf0a99d74f500e14357969c91b
Sam Cambridge <sam.cambridge@gmail.com>
2018-03-04 15:59:53 +0000

    script gets all mobs  then drops

---
+++ data_scraper.rb
@@ -0,0 +1,61 @@
+# quick n' dirty procedural script to fetch the mob drops from the site
+# and create a json file
+
+require 'capybara'
+require 'capybara/poltergeist'
+
+# setup
+Capybara.register_driver :selenium do |app|
+  Capybara::Selenium::Driver.new(app, :browser => :chrome)
+end
+
+Capybara.current_driver = :selenium
+page = Capybara::Session.new(:selenium)
+
+# visit the main directory
+page.visit 'https://www.helbreath.net/res_monster'
+# find all urls in the page
+mob_pages = page.all('.postcontent a').map { |a| a['href'] }
+# init data hash
+data = {}
+
+forget_about = ["weapons", "armours", "rares", "semi-"]
+
+# loop pages
+mob_pages.each do |mob_page|
+  # get npc name
+  page.visit mob_page
+  npc = mob_page.gsub('https://www.helbreath.net/res_monster?m=', '')
+  puts npc
+  table = page.find('.npcdrop')
+  all_text = table.find_all('td')
+
+  cleaned_drops = []
+
+  all_text.each do |td|
+    html = page.execute_script("return arguments[0].innerHTML;", td)
+
+    if (html.include?("div"))
+      drops = html.gsub(/<div.*?>|<\/div>/, '').split('<br>')
+    else
+      drops = html.split('<br>')
+    end
+
+    drops.each do |drop|
+      drop = drop.downcase
+      forget_about.each do |phrase|
+        drop.gsub!(/#{phrase}/,'')
+      end
+
+      unless drop == ""
+        cleaned_drops << drop
+      end
+    end
+
+  end
+
+    data[npc.to_sym] = cleaned_drops
+    puts cleaned_drops
+end
+
+raise data.inspect
