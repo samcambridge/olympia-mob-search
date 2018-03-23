fdb6b676222b48ac1332d1d141bd7d013ad0dd9e
Sam Cambridge <sam.cambridge@gmail.com>
2018-03-23 14:35:32 +0000

    start of calculator

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import "./App.css";
 
 import ItemSearch from "./Components/ItemSearch.js";
 import NpcView from "./Components/NpcView.js";
+import Calculator from "./Components/Calculator.js";
 
 class App extends Component {
   render() {
@@ -12,14 +13,25 @@ class App extends Component {
       <Router>
         <div className="App">
           <header className="App-header">
-            Helbreath Olympia NPC Drop Search
+            Helbreath Olympia Tools<br />
+            <small>Developed by e11even &amp; ocambridge</small>
           </header>
+
+          <nav style={{ textAlign: "center", marginTop: "20px" }}>
+            <Link style={{ marginRight: 20 }} to={`/`}>
+              NPC Drop Search
+            </Link>
+            <Link to={`/calculator`}>
+              Helbreath Olympia Character Simulator
+            </Link>
+          </nav>
 
           <Route exact={true} path="/" component={ItemSearch} />
           <Route
             path="/m/:npc"
             render={({ match }) => <NpcView npc={match.params.npc} />}
           />
+          <Route exact={true} path="/calculator" component={Calculator} />
         </div>
       </Router>
     );
---
+++ src/Components/Calculator.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Calculator extends Component {
+  render() {
+    return <div className="App-search">Calculator coming soon</div>;
+  }
+}
+
+export default Calculator;
