a4e4c347d12b020d67847983b6b35924b153c1c3
Sam Cambridge <sam.cambridge@gmail.com>
2018-05-22 09:19:12 +0100

    remove the word uncommon

--- data_scraper.rb
+++ data_scraper.rb
@@ -19,7 +19,7 @@ mob_pages = page.all('.postcontent a').map { |a| a['href'] }
 # init data hash
 data = {}
 
-forget_about = ['weapons', 'armours', 'rares', 'semi-']
+forget_about = ['weapons', 'armours', 'rares', 'semi-', 'uncommon']
 
 # loop pages
 mob_pages.each do |mob_page|
